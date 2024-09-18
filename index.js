54efa30828effcfa6dbf9467a9c60315236f75b0
wellingtonmr <wellingtonmourar@gmail.com>
2024-09-18 12:18:17 -0300

    Finished the course
    I might go back to this so I can make a proper game, that show the cards, do bets and earn chips

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let player = {
+	name: "Wellington",
+	chips: 145
+}
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,6 +11,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function startGame() {
 	isAlive = true
